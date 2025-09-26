cd7453459738a765d7f4477880e1609282971c02
jjuchan <yjc7241@naver.com>
2025-09-26 16:09:43 +0900

    kakao button add

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -45,6 +45,20 @@ export default function ClientLayout({
               로그인
             </Link>
           )}
+          <>
+            <Link
+              href="/members/login"
+              className="p-2 rounded hover:bg-gray-100"
+            >
+              로그인
+            </Link>
+            <a
+              href="http://localhost:8080/oauth2/authorization/kakao"
+              className="p-2 rounded hover:bg-gray-100"
+            >
+              카카오 로그인
+            </a>
+          </>
         </nav>
       </header>
       <main className="flex-1 flex flex-col">{children}</main>
